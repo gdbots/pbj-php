7ad6aeb2db37c73ffceefa8fa1cadb83a83011c7
Greg Brown <greg@12dnetworks.com>
2015-02-08 11:16:59 -0800

    small cleanup

--- src/Gdbots/Pbj/Extension/ValueObject.php
+++ src/Gdbots/Pbj/Extension/ValueObject.php
@@ -4,6 +4,10 @@ namespace Gdbots\Pbj\Extension;
 
 use Gdbots\Pbj\Message;
 
+/*
+ * WIP, not actually a true value object as it still has mutators
+ * even though it can be frozen.
+ */
 interface ValueObject extends Message
 {
     /**
