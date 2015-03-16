a1ff154f870f7cf8700cc53081e5046e9601da42
Greg Brown <greg@12dnetworks.com>
2015-03-16 10:31:33 -0700

    add correlateWith method

--- src/Gdbots/Pbj/HasCorrelator.php
+++ src/Gdbots/Pbj/HasCorrelator.php
@@ -5,6 +5,16 @@ namespace Gdbots\Pbj;
 interface HasCorrelator
 {
     /**
+     * Correlates a message to another that also has a correlator with
+     * an optional tag to qualify the reference.
+     *
+     * @param HasCorrelator $other
+     * @param string $tag
+     * @return static
+     */
+    public function correlateWith(HasCorrelator $other, $tag = null);
+
+    /**
      * @return bool
      */
     public function hasCorrelator();
--- src/Gdbots/Pbj/HasCorrerlatorTrait.php
+++ src/Gdbots/Pbj/HasCorrerlatorTrait.php
@@ -6,9 +6,32 @@ namespace Gdbots\Pbj;
  * @method bool has(string $fieldName)
  * @method mixed get(string $fieldName)
  * @method static setSingleValue(string $fieldName, mixed $value)
+ * @method static clear(string $fieldName)
+ * @method bool isFrozen()
  */
 trait HasCorrerlatorTrait
 {
+    /**
+     * @param HasCorrelator $other
+     * @param string $tag
+     * @return static
+     */
+    public function correlateWith(HasCorrelator $other, $tag = null)
+    {
+        if ($this->isFrozen()) {
+            return $this;
+        }
+
+        if (!$other->hasCorrelator()) {
+            if ($other instanceof GeneratesMessageRef) {
+                return $this->setCorrelator($other->generateMessageRef($tag));
+            }
+            return $this;
+        }
+
+        return $this->setCorrelator($other->getCorrelator());
+    }
+
     /**
      * @return bool
      */
