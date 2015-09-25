9f0ee216b01258abffd7277e2df667c3b15c2345
Greg Brown <greg@12dnetworks.com>
2015-09-24 22:52:07 -0700

    re #15 updated fixture loader in unit tests to account for date time clone issue with milliseconds

--- tests/FixtureLoader.php
+++ tests/FixtureLoader.php
@@ -2,6 +2,7 @@
 
 namespace Gdbots\Tests\Pbj;
 
+use Gdbots\Common\Util\DateUtils;
 use Gdbots\Pbj\Serializer\JsonSerializer;
 use Gdbots\Pbj\Serializer\Serializer;
 use Gdbots\Tests\Pbj\Fixtures\EmailMessage;
@@ -34,6 +35,13 @@ trait FixtureLoader
             $json = file_get_contents(__DIR__ . '/Fixtures/email-message.json');
             self::$emailMessageFixture = $this->getSerializer()->deserialize($json);
         }
-        return clone self::$emailMessageFixture;
+
+        $message = clone self::$emailMessageFixture;
+
+        // fixme: handle clipping microseconds due to clone issue.  see issue #15
+        $date = \DateTime::createFromFormat(DateUtils::ISO8601, '2014-12-25T12:12:00.123456+00:00');
+        $message->setSingleValue('date_sent', $date);
+
+        return $message;
     }
 }
